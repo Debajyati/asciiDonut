2a9c7f5849c4bff59bd107059c633f2b74f3b315
Debajyati Dey <ddebajyati@gmail.com>
2025-01-12 20:47:34 +0530

    ditch esbuild command from build script

--- tsc.sh
+++ tsc.sh
@@ -4,10 +4,8 @@
 node_modules/.bin/tsc index.ts --module esnext --moduleResolution Node --outDir ./dist/ -d true --declarationDir ./types
 rm dist/index.mjs
 mv dist/index.js dist/index.mjs
-node_modules/.bin/esbuild dist/index.mjs --outfile=dist/index.mjs --bundle --allow-overwrite
 
 # For commonjs Export
 node_modules/.bin/tsc index.ts --module commonjs --outDir ./dist/
 rm dist/index.cjs
 mv dist/index.js dist/index.cjs
-node_modules/.bin/esbuild dist/index.cjs --outfile=dist/index.cjs --bundle --allow-overwrite
